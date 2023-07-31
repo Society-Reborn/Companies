ad340e7f17fd3bac57ee8665c5245a348604ab3f
Yassev <135447301+KalypsoExists@users.noreply.github.com>
2023-07-31 22:42:58 +0530

    How Did We Get Here?
    The begininiginiging

---
+++ src/main/java/net/societyreborn/companies/Companies.java
@@ -0,0 +1,18 @@
+package net.societyreborn.companies;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Companies extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Zed's gay and Tsko's dumb
+        // Official full form of "FEAR":
+        // FUCK EVERYTHING AND REPRODUCE!!!
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
